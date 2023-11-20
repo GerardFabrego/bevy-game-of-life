25471d24461734d995a10627fd23564aa7b1133f
Gerard Fabregó <gerardfabrego@gmail.com>
2023-11-20 07:30:18 +0100

    Set window title and resolution

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowResolution};
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: WindowResolution::new(1024f32, 720f32),
+                title: String::from("Game of Life"),
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
 }
